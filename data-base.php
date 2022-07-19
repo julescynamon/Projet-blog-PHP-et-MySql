d665bf279e69151a4657d813aa5a7e925422f7c1
cynamon jules <julescweb@mail.com>
2022-07-19 16:40:08 +0200

    on recupere les articles de la bdd

--- data-base.php
+++ data-base.php
@@ -4,7 +4,15 @@ $dns = 'mysql:host=localhost;dbname=blog';
 $user = 'root';
 $password = '6689JUles';
 
-$pdo = new PDO($dns, $user, $password, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-]);
+
+
+try {
+    $pdo = new PDO($dns, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    echo 'Error :' . $e->getMessage();
+}
+
+return $pdo;
--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-$filename = __DIR__ . '/data/articles.json';
-$articles = [];
+$pdo = require_once 'data-base.php';
+$statement = $pdo->prepare('SELECT * FROM articles');
+$statement->execute();
+$articles = $statement->fetchAll();
 $categories = [];
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $selectedCat = $_GET['cat'] ?? '';
 
-if (file_exists($filename)) {
-    $articles = json_decode(file_get_contents($filename), true) ?? [];
+if (count($articles)) {
+
     $cattmp = array_map(fn ($a) => $a['category'],  $articles);
     $categories = array_reduce($cattmp, function ($acc, $cat) {
         if (isset($acc[$cat])) {
